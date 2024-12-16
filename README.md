9be3ff5df8b5b2c3be38d44c5256be000147bcdf
Trong Nhan <92970779+NMTNhan@users.noreply.github.com>
2024-12-16 20:20:08 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+## Backend
+1. Open terminal (CTRL+`)
+2. cd backend
+3. npm i
+4. npm start
+
+## Frontend
+1. Open terminal (CTRL+`)
+2. npm i
+3. npm start
+
+Run both at the same time still missing feature but the player is working properly if you use Postman
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#51EEFC",
+        secondary: "#297DFD",
+        darkBlue: "#0015FF",
+        white: "#FFFFFF",
+        gray: "#D9D9D9",
+        blackBlue: "#00053E",
+        red: "#FF0000",
+      },
+    },
+  },
+  plugins: [],
+};
